338b0f39f92cd42c7a18df7f8d7e432ae3962db8
DDevjourney <123167384+DDevjourney@users.noreply.github.com>
2024-12-25 18:11:45 +0100

    Updade 0.01
    Try .cs in laptop

--- EstructurasArrays/EstructurasArrays/Program.cs
+++ EstructurasArrays/EstructurasArrays/Program.cs
@@ -35,7 +35,7 @@ namespace EstructurasArrays
             //int[] numeros;
             //numeros = new int[5];
 
-                       
+
             //Console.WriteLine("Introduce el primero número");
             //numeros[0] = Int32.Parse(Console.ReadLine());
 
@@ -58,41 +58,52 @@ namespace EstructurasArrays
             //Ejercicio 3: Encuentra el número más grande
             //Crea un programa que pida al usuario que ingrese 10 números enteros y los guarde en un array. Luego, encuentra y muestra el número más grande en el array.
 
-            //Ejercicio 4: Invertir un array
-            //Escribe un programa que pida al usuario 5 palabras y las almacene en un array.Luego, invierte el orden del array y muestra las palabras en orden inverso.
+            int[] numeros;
+            numeros = new int[10];
 
-            //Ejercicio 5: Contar ocurrencias
-            //Crea un programa que pida al usuario ingresar 10 números enteros y los almacene en un array.
-            //Luego, solicita otro número y cuenta cuántas veces aparece ese número en el array.
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Introduce un número");
+                numeros[i] = Int32.Parse(Console.ReadLine());
+            }
 
-            //Ejercicio 6: Tabla de multiplicar en un array
-            //Crea un programa que genere automáticamente un array con los resultados de la tabla de multiplicar del número 5(del 5x1 al 5x10).
-            //Luego, imprime todos los valores del array.
 
-            //Ejercicio 7: Ordenar un array
-            //Escribe un programa que solicite al usuario 5 números enteros, los almacene en un array, los ordene de menor a mayor y luego los muestre en la consola.
 
-            //Ejercicio 8: Array bidimensional
-            //Crea un programa que declare una matriz(array bidimensional) de 2 filas y 3 columnas.
-            //Llénala con números ingresados por el usuario y luego muestra todos los elementos en la consola.
+                //Ejercicio 4: Invertir un array
+                //Escribe un programa que pida al usuario 5 palabras y las almacene en un array.Luego, invierte el orden del array y muestra las palabras en orden inverso.
 
-            //Ejercicio 9: Sumar dos arrays
-            //Escribe un programa que cree dos arrays de tamaño 5 con números ingresados por el usuario.
-            //Luego, suma los valores correspondientes de ambos arrays y almacena el resultado en un tercer array.Finalmente, imprime el array resultante.
+                //Ejercicio 5: Contar ocurrencias
+                //Crea un programa que pida al usuario ingresar 10 números enteros y los almacene en un array.
+                //Luego, solicita otro número y cuenta cuántas veces aparece ese número en el array.
 
-            //Ejercicio 10: Verificar si un número está en el array
-            //Crea un programa que solicite al usuario 10 números para llenar un array.Luego, pide al usuario otro número y verifica si este número está en el array.
-            //Muestra un mensaje indicando si está o no.
+                //Ejercicio 6: Tabla de multiplicar en un array
+                //Crea un programa que genere automáticamente un array con los resultados de la tabla de multiplicar del número 5(del 5x1 al 5x10).
+                //Luego, imprime todos los valores del array.
 
-            //Ejercicio 11: Concatenar palabras
-            //Escribe un programa que pida al usuario 3 palabras, las almacene en un array y luego las concatene en una sola cadena de texto, separadas por espacios.
+                //Ejercicio 7: Ordenar un array
+                //Escribe un programa que solicite al usuario 5 números enteros, los almacene en un array, los ordene de menor a mayor y luego los muestre en la consola.
 
-            //Ejercicio 12: Encuentra el índice
-            //Crea un programa que almacene 5 nombres en un array y solicite al usuario que ingrese uno de esos nombres.
-            //Muestra el índice en el que se encuentra el nombre ingresado.Si el nombre no está en el array, muestra un mensaje indicando que no se encontró.
+                //Ejercicio 8: Array bidimensional
+                //Crea un programa que declare una matriz(array bidimensional) de 2 filas y 3 columnas.
+                //Llénala con números ingresados por el usuario y luego muestra todos los elementos en la consola.
 
-            //Ejercicio 13: Array de caracteres
-            //Escribe un programa que solicite una palabra al usuario y la almacene como un array de caracteres. Luego, muestra cada carácter en una línea diferente.
+                //Ejercicio 9: Sumar dos arrays
+                //Escribe un programa que cree dos arrays de tamaño 5 con números ingresados por el usuario.
+                //Luego, suma los valores correspondientes de ambos arrays y almacena el resultado en un tercer array.Finalmente, imprime el array resultante.
+
+                //Ejercicio 10: Verificar si un número está en el array
+                //Crea un programa que solicite al usuario 10 números para llenar un array.Luego, pide al usuario otro número y verifica si este número está en el array.
+                //Muestra un mensaje indicando si está o no.
+
+                //Ejercicio 11: Concatenar palabras
+                //Escribe un programa que pida al usuario 3 palabras, las almacene en un array y luego las concatene en una sola cadena de texto, separadas por espacios.
+
+                //Ejercicio 12: Encuentra el índice
+                //Crea un programa que almacene 5 nombres en un array y solicite al usuario que ingrese uno de esos nombres.
+                //Muestra el índice en el que se encuentra el nombre ingresado.Si el nombre no está en el array, muestra un mensaje indicando que no se encontró.
+
+                //Ejercicio 13: Array de caracteres
+                //Escribe un programa que solicite una palabra al usuario y la almacene como un array de caracteres. Luego, muestra cada carácter en una línea diferente.
+            }
         }
-    }
 }
